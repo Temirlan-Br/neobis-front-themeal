a8312be4e54e7f8034fd7d2150caeb432bb097c2
Temirlan-Br <barakov.temirlan@gmail.com>
2023-11-28 16:54:22 +0600

    Add section about food and style for this

--- src/components/FoodInfo.jsx
+++ src/components/FoodInfo.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+// import { Link } from 'react-router-dom';
 
 const FoodInfo = () => {
+  const [food, setFood] = useState([]);
+  const API = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+  useEffect(() => {
+    axios.get(API).then((repos) => {
+      setFood(repos.data.meals);
+    });
+  }, []);
+
   return (
     <div>
-      <div className="food__block">
-        <div className="info__block">
-          <p>Meal of the day</p>
-          <h2>Manty</h2>
-          <p>Koi | Kyrgyzstan</p>
+      {food.length > 0 ? (
+        <div className="food__block">
+          <div className="info__block">
+            <p className="heading">Meal of the day</p>
+            <h2 className="title">{food[0].strMeal}</h2>
+            <p className="subtitle">
+              {food[0].strCategory} | {food[0].strArea}
+            </p>
+          </div>
+          <div className="img__block">
+            <img src={food[0].strMealThumb} alt="meal" />
+          </div>
         </div>
-        <div className="img__block">
-          <img
-            src="https://plovnaya1.com/thumb/2/0Z-apnJ6UXPUrZuMK-HBXw/450r450/d/manty_s_baraninoj.jpg"
-            alt=""
-          />
-        </div>
-      </div>
+      ) : (
+        <h3 className="subtitle">Loading...</h3>
+      )}
     </div>
   );
 };
