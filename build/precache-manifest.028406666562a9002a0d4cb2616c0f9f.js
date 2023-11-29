ea23d5171a25b765983ae7c4af722611ed2995aa
Temirlan-Br <barakov.temirlan@gmail.com>
2023-11-29 16:06:45 +0600

    Add production build

---
+++ build/precache-manifest.028406666562a9002a0d4cb2616c0f9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc5495192abd82a117ccaa258396160",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfe42f08b795c983e665",
+    "url": "/static/css/main.de59e914.chunk.css"
+  },
+  {
+    "revision": "0aaa2321cc2ee2f1ddba",
+    "url": "/static/js/2.c1a24f63.chunk.js"
+  },
+  {
+    "revision": "8ba8997f9f8a1b4cb4c9278bd845eca8",
+    "url": "/static/js/2.c1a24f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfe42f08b795c983e665",
+    "url": "/static/js/main.58cc969b.chunk.js"
+  },
+  {
+    "revision": "70dc06a105f8f83d7a11",
+    "url": "/static/js/runtime-main.9f1d15a3.js"
+  }
+]);
